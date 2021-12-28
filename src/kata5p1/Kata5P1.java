bc946778e04d3f39b50dd708d9a0a6b312676fc2
Fran <Fran@DESKTOP-4HUTDEP>
2021-12-28 19:35:13 +0000

    Kata5P1 v2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,10 +1,14 @@
 package kata5p1;
 
-public class Kata5P1 {
+import java.util.List;
 
+public class Kata5P1 {
+    
     public static void main(String[] args) {
         SelectApp app = new SelectApp();
         app.selectAll();
+        
+        CrearTabla.createNewTable();
     }
 
 }
